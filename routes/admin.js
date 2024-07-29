f49fa9cc3c0d142dc7e42732c9dbed3e4b655402
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-07-29 14:23:30 -0400

    Added 404 not found page and template for add product page.

--- routes/admin.js
+++ routes/admin.js
@@ -10,9 +10,8 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product'});
 });
-
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title })
